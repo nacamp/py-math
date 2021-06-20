20f6bddedfa87f981bec9c2a3d6074871c571fc0
najimmy <najimmy@gmail.com>
2021-06-20 14:18:31 +0900

    add mul change variable name

--- pyxmath/finite_mono_polynomial.py
+++ pyxmath/finite_mono_polynomial.py
@@ -5,6 +5,7 @@ class FiniteMonoPolynomialError(Exception):
     def __init__(self, message):
         self.message = message
 
+
 class FiniteMonoPolynomial():
     @staticmethod
     def neg(a):
@@ -23,29 +24,31 @@ class FiniteMonoPolynomial():
         self.deg = len(coefs) - 1
         self.mod = mod
         self.fields = []
-        # ex: x^3+2x+1=0 (mod3) :  x^3=0x^2-2x-1 : [2,1,0]
-        self.finite_coefs = coefs[:]
-        self.finite_coefs.pop()
-        self.finite_coefs = [x * (-1) % self.mod for x in self.finite_coefs]
+        # ex: x^3+2x+1=0 (mod3) : x^3=0x^2-2x-1 : [2,1,0] :  left_coef=1, right_coefs=[2,1,0]
+        self.right_coefs = coefs[:]
+        self.right_coefs.pop()
+        self.right_coefs = [x * (-1) % self.mod for x in self.right_coefs]
 
-    def add(self, a, b):
-        if len(a) > self.deg or len(b) > self.deg:
-            raise FiniteMonoPolynomialError(f'exceed the maximum degree {self.deg}')
-        if isinstance(a, int):
-            a = [a]
-        if isinstance(b, int):
-            b = [b]
-        return [sum(x) % self.mod for x in itertools.zip_longest(a, b, fillvalue=0)]
+    def add(self, r_a, r_b):
+        '''add
 
-    def sub(self, a, b):
-        if len(a) > self.deg or len(b) > self.deg:
-            raise FiniteMonoPolynomialError(f'exceed the maximum degree {self.deg}')
-        if isinstance(a, int):
-            a = [a]
-        if isinstance(b, int):
-            b = [b]
-        aa = list(a[:])
-        bb = list(b[:])
+        :param r_a: right_coefs
+        :param r_b: right_coefs
+        :return:
+        '''
+        self._validate(r_a, r_b)
+        return [sum(x) % self.mod for x in itertools.zip_longest(r_a, r_b, fillvalue=0)]
+
+    def sub(self, r_a, r_b):
+        '''sub
+
+        :param r_a: right_coefs
+        :param r_b: right_coefs
+        :return:
+        '''
+        self._validate(r_a, r_b)
+        aa = list(r_a[:])
+        bb = list(r_b[:])
         for i in range(max(len(aa), len(bb))):
             if len(aa) == i:
                 aa.append(0)
@@ -53,3 +56,42 @@ class FiniteMonoPolynomial():
                 bb.append(0)
             aa[i] = (aa[i] - bb[i]) % self.mod
         return aa
+
+    def mul(self, r_a, r_b):
+        '''mul
+
+        :param r_a: right_coefs
+        :param r_b: right_coefs
+        :return:
+        '''
+        self._validate(r_a, r_b)
+        r_coefs_len = len(self.right_coefs)
+        over_coefs = self._mul_coef(r_a, r_b)
+        coefs = over_coefs[0:r_coefs_len]
+        l = len(over_coefs)
+        while l > r_coefs_len:
+            over_coefs = self._mul_coef(self.right_coefs, over_coefs[r_coefs_len:])
+            l = len(over_coefs)
+            coefs = [sum(x) for x in itertools.zip_longest(over_coefs[0:r_coefs_len], coefs, fillvalue=0)]
+        return ([x % self.mod for x in coefs])
+
+    def _mul_coef(self, r_a, r_b):
+        aa = list(r_a[:])
+        bb = list(r_b[:])
+        while len(aa) > 0 and aa[-1] == 0:
+            aa.pop()
+        while len(bb) > 0 and bb[-1] == 0:
+            bb.pop()
+        r = [0] * (len(aa) + len(bb) - 1)
+        for i, c in enumerate(bb):
+            for j in range(len(aa)):
+                r[i + j] += c * aa[j]
+        return r
+
+    def _validate(self, r_a, r_b):
+        if isinstance(r_a, int):
+            r_a = [r_a]
+        if isinstance(r_b, int):
+            r_b = [r_b]
+        if len(r_a) > self.deg or len(r_b) > self.deg:
+            raise FiniteMonoPolynomialError(f'exceed the maximum degree {self.deg}')
--- pyxmath/tests/test_finite_mono_polynomial.py
+++ pyxmath/tests/test_finite_mono_polynomial.py
@@ -3,11 +3,31 @@ from pyxmath.finite_mono_polynomial import *
 
 
 def test_add_sub():
-    field = FiniteMonoPolynomial([4, 1, 0, 1], 11)
+    poly = FiniteMonoPolynomial([4, 1, 0, 1], 11)
     with pytest.raises(FiniteMonoPolynomialError):
-        field.add([1, 10, 10, 10], [2, 2, 2])
-    assert field.add([10, 10, 10], [2, 2, 2]) == [1, 1, 1]
+        poly.add([1, 10, 10, 10], [2, 2, 2])
+    assert poly.add([10, 10, 10], [2, 2, 2]) == [1, 1, 1]
 
     with pytest.raises(FiniteMonoPolynomialError):
-        field.sub([1, 2, 2, 2], [10, 10, 10])
-    assert field.sub([2, 2, 2], [10, 10, 10]) == [3, 3, 3]
+        poly.sub([1, 2, 2, 2], [10, 10, 10])
+    assert poly.sub([2, 2, 2], [10, 10, 10]) == [3, 3, 3]
+
+def test_mul():
+    poly = FiniteMonoPolynomial([4, 1, 0, 1], 11)
+    assert poly.mul([1, 1, 1], [2, 2, 2]) == [8, 3, 4]
+    '''
+    1u^3+0u^3+1u^1+4u^0 => u^3 = 10u^+7
+      
+                         2 2 2
+                        x1 1 1
+                        ------
+         2u^4+4u^3+6u^2+4u^1+2
+      2u^4=2u*u^3=20u*(10u^+7)
+      4u^3=4u^3  =  4*(10u^+7)
+                        10 7
+                       x 2 4
+                       ------
+               20u^2+54u^1+28    
+    6u^2+4u^1+2 + 20u^2+54u^1+28 = 26u^2+58u^1+30 = 4u^2+3u^1+8 (mod 11)
+    => [8, 3, 4]
+    '''
